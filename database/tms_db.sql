62775740fac5074d7c3697c9ecc945e766400f23
Bayu ID <9622690+bayuihsan@users.noreply.github.com>
2022-08-15 20:10:33 +0700

    Update tms_db.sql

--- database/tms_db.sql
+++ database/tms_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 15, 2022 at 01:34 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Waktu pembuatan: 15 Agu 2022 pada 15.09
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,66 +24,266 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `par_user_role`
+-- Struktur dari tabel `par_user_role`
 --
--- Error reading structure for table tms_db.par_user_role: #1932 - Table &#039;tms_db.par_user_role&#039; doesn&#039;t exist in engine
--- Error reading data for table tms_db.par_user_role: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `tms_db`.`par_user_role`&#039; at line 1
+
+CREATE TABLE `par_user_role` (
+  `id_role` int(11) NOT NULL,
+  `nama_role` varchar(100) DEFAULT NULL,
+  `is_active` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `project_list`
+-- Struktur dari tabel `project_list`
 --
--- Error reading structure for table tms_db.project_list: #1932 - Table &#039;tms_db.project_list&#039; doesn&#039;t exist in engine
--- Error reading data for table tms_db.project_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `tms_db`.`project_list`&#039; at line 1
+
+CREATE TABLE `project_list` (
+  `id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(2) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `manager_id` int(30) NOT NULL,
+  `user_ids` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `project_list`
+--
+
+INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`) VALUES
+(1, 'Sample Project', '														&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, metus vitae malesuada mollis, urna nisi luctus ligula, vitae volutpat massa eros eu ligula. Nunc dui metus, iaculis id dolor non, luctus tristique libero. Aenean et sagittis sem. Nulla facilisi. Mauris at placerat augue. Nullam porttitor felis turpis, ac varius eros placerat et. Nunc ut enim scelerisque, porta lacus vitae, viverra justo. Nam mollis turpis nec dolor feugiat, sed bibendum velit placerat. Etiam in hendrerit leo. Nullam mollis lorem massa, sit amet tincidunt dolor lacinia at.&lt;/span&gt;												', 5, '2020-11-03', '2021-01-20', 2, '3,4,5', '2020-12-03 09:56:56'),
+(2, 'Sample Project 102', 'Sample Only', 0, '2020-12-02', '2020-12-31', 2, '3', '2020-12-03 13:51:54');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `system_settings`
+-- Struktur dari tabel `system_settings`
 --
--- Error reading structure for table tms_db.system_settings: #1932 - Table &#039;tms_db.system_settings&#039; doesn&#039;t exist in engine
--- Error reading data for table tms_db.system_settings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `tms_db`.`system_settings`&#039; at line 1
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'Task Management System', 'info@sample.comm', '+6948 8542 623', '2102  Caldwell Road, Rochester, New York, 14608', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_role`
+-- Struktur dari tabel `tabel_menu`
 --
--- Error reading structure for table tms_db.tabel_role: #1932 - Table &#039;tms_db.tabel_role&#039; doesn&#039;t exist in engine
--- Error reading data for table tms_db.tabel_role: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `tms_db`.`tabel_role`&#039; at line 1
+
+CREATE TABLE `tabel_menu` (
+  `id_menu` int(11) NOT NULL,
+  `nama_menu` varchar(100) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  `icon` varchar(50) DEFAULT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `have_child` int(11) DEFAULT NULL,
+  `is_active` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `table_menu`
+-- Struktur dari tabel `tabel_role`
 --
--- Error reading structure for table tms_db.table_menu: #1932 - Table &#039;tms_db.table_menu&#039; doesn&#039;t exist in engine
--- Error reading data for table tms_db.table_menu: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `tms_db`.`table_menu`&#039; at line 1
+
+CREATE TABLE `tabel_role` (
+  `id_tabel_role` int(11) DEFAULT NULL,
+  `id_menu` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `task_list`
+-- Struktur dari tabel `task_list`
 --
--- Error reading structure for table tms_db.task_list: #1932 - Table &#039;tms_db.task_list&#039; doesn&#039;t exist in engine
--- Error reading data for table tms_db.task_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `tms_db`.`task_list`&#039; at line 1
+
+CREATE TABLE `task_list` (
+  `id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `task` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `task_list`
+--
+
+INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `status`, `date_created`) VALUES
+(1, 1, 'Sample Task 1', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Fusce ullamcorper mattis semper. Nunc vel risus ipsum. Sed maximus dapibus nisl non laoreet. Pellentesque quis mauris odio. Donec fermentum facilisis odio, sit amet aliquet purus scelerisque eget.&amp;nbsp;&lt;/span&gt;													', 3, '2020-12-03 11:08:58'),
+(2, 1, 'Sample Task 2', '								Sample Task 2													', 3, '2020-12-03 13:50:15'),
+(3, 2, 'Task Test', '				Sample			', 2, '2020-12-03 13:52:25'),
+(4, 2, 'test 23', 'Sample test 23', 1, '2020-12-03 13:52:40');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
--- Error reading structure for table tms_db.users: #1932 - Table &#039;tms_db.users&#039; doesn&#039;t exist in engine
--- Error reading data for table tms_db.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `tms_db`.`users`&#039; at line 1
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1 = admin, 2 = staff',
+  `avatar` text NOT NULL DEFAULT 'no-image-available.png',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
+(1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 'no-image-available.png', '2020-11-26 10:57:04'),
+(2, 'John', 'Smith', 'jsmith@sample.com', 'e10adc3949ba59abbe56e057f20f883e', 2, '1606978560_avatar.jpg', '2020-12-03 09:26:03'),
+(3, 'Claire', 'Blake', 'cblake@sample.com', 'e10adc3949ba59abbe56e057f20f883e', 3, '1606958760_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 09:26:42'),
+(4, 'George', 'Wilson', 'gwilson@sample.com', 'e10adc3949ba59abbe56e057f20f883e', 3, '1606963560_avatar.jpg', '2020-12-03 10:46:41'),
+(5, 'Mike', 'Williams', 'mwilliams@sample.com', '3cc93e9a6741d8b40460457139cf8ced', 3, '1606963620_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 10:47:06');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_productivity`
+-- Struktur dari tabel `user_productivity`
 --
--- Error reading structure for table tms_db.user_productivity: #1932 - Table &#039;tms_db.user_productivity&#039; doesn&#039;t exist in engine
--- Error reading data for table tms_db.user_productivity: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `tms_db`.`user_productivity`&#039; at line 1
+
+CREATE TABLE `user_productivity` (
+  `id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `task_id` int(30) NOT NULL,
+  `comment` text NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `user_id` int(30) NOT NULL,
+  `time_rendered` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_productivity`
+--
+
+INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subject`, `date`, `start_time`, `end_time`, `user_id`, `time_rendered`, `date_created`) VALUES
+(1, 1, 1, '							&lt;p&gt;Sample Progress&lt;/p&gt;&lt;ul&gt;&lt;li&gt;Test 1&lt;/li&gt;&lt;li&gt;Test 2&lt;/li&gt;&lt;li&gt;Test 3&lt;/li&gt;&lt;/ul&gt;																			', 'Sample Progress', '2020-12-03', '08:00:00', '10:00:00', 1, 2, '2020-12-03 12:13:28'),
+(2, 1, 1, '							Sample Progress						', 'Sample Progress 2', '2020-12-03', '13:00:00', '14:00:00', 1, 1, '2020-12-03 13:48:28'),
+(3, 1, 2, '							Sample						', 'Test', '2020-12-03', '08:00:00', '09:00:00', 5, 1, '2020-12-03 13:57:22'),
+(4, 1, 2, 'asdasdasd', 'Sample Progress', '2020-12-02', '08:00:00', '10:00:00', 2, 2, '2020-12-03 14:36:30'),
+(5, 1, 1, 'tes', 'subject 1', '2022-08-14', '16:15:00', '16:15:00', 1, 0, '2022-08-14 16:15:17'),
+(6, 2, 3, 'okey', 'subject 1', '2022-08-14', '16:20:00', '16:25:00', 1, 0.0833333, '2022-08-14 16:20:43');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `par_user_role`
+--
+ALTER TABLE `par_user_role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indeks untuk tabel `project_list`
+--
+ALTER TABLE `project_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tabel_menu`
+--
+ALTER TABLE `tabel_menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indeks untuk tabel `task_list`
+--
+ALTER TABLE `task_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_productivity`
+--
+ALTER TABLE `user_productivity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `par_user_role`
+--
+ALTER TABLE `par_user_role`
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `project_list`
+--
+ALTER TABLE `project_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tabel_menu`
+--
+ALTER TABLE `tabel_menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `task_list`
+--
+ALTER TABLE `task_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_productivity`
+--
+ALTER TABLE `user_productivity`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
